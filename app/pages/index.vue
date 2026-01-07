128cf1fec5a2a807657d66592f18b13b74530e82
Farnabaz <farnabaz@gmail.com>
2026-01-07 14:29:36 +0100

    up

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { queryCollection } from '#imports'
 const siteConfig = useAsyncData('site-config', async () => queryCollection('siteConfig').first())
 </script>
 <template>
